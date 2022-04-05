b969082ee7e634ea51d03c2327bc05258afaaa63
natoron <78657730+natoron@users.noreply.github.com>
2022-04-05 18:29:17 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask (__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route ('/')
 def aDePF():
